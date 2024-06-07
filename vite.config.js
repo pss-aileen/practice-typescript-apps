5a1688e9ce02ea76acc0336c7ce1a9007275f92c
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-06-08 00:07:18 +0900

    📂 chore: fix base path @ vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/pss-aileen.github.io/practice-typescript-apps/",
   build: {
     rollupOptions: {
       input: {
