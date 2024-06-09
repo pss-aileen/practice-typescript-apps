a7f6c65e8a2cc32758fae12d02abefad98c88701
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-06-09 22:52:41 +0900

    📂 chore: favicon test setting

---
+++ random-quote/src/main.ts
@@ -0,0 +1,8 @@
+// import "tailwindcss/tailwind.css";
+
+// fetch("https://dummyjson.com/test")
+fetch("https://dummyjson.com/quotes/random")
+  // starts the process of fetching: https://developer.mozilla.org/en-US/docs/Web/API/fetch
+  .then((res) => res.json())
+  // when promise return `fulfilled` , do `res` and then, return res.json()
+  .then(console.log);
