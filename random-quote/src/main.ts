794f6888f4dfd3dbc31efda02e758b635ac58684
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-06-11 10:59:17 +0900

    🚧 wip: implement by myself #18
    next refactor

--- random-quote/src/main.ts
+++ random-quote/src/main.ts
@@ -1,8 +1,15 @@
-// import "tailwindcss/tailwind.css";
-
 // fetch("https://dummyjson.com/test")
 fetch("https://dummyjson.com/quotes/random")
   // starts the process of fetching: https://developer.mozilla.org/en-US/docs/Web/API/fetch
   .then((res) => res.json())
   // when promise return `fulfilled` , do `res` and then, return res.json()
-  .then(console.log);
+  .then((data) => {
+    console.log(data);
+    const quoteDom = document.getElementById("quote") as HTMLElement;
+    const authorDom = document.getElementById("author") as HTMLElement;
+    const quoteIdDom = document.getElementById("quote-id") as HTMLElement;
+
+    quoteIdDom.textContent = data.id;
+    quoteDom.textContent = data.quote;
+    authorDom.textContent = data.author;
+  });
